04edc9a62bbc94fda3afb1e9e42c3a296efb2a98
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-04-12 00:15:23 +0200

    0-pascal_triangle.py: Made efficient

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
     """
     a function that returns a list
@@ -6,19 +10,23 @@ def pascal_triangle(n):
        . Returns an empty list if n <= 0
        . assume n will be always an integer
     """
+    pascal_tri = []
+
     if n <= 0:
         return []
 
-    pascal_tri = [[1]]
+    for i in range(n):
+        if (i == 0):
+            pascal_tri.append([1])
+        else:
+            cur_row = []
+            for j in range(i + 1):
+                if (j == 0 or j == i):
+                    cur_row.append(1)
+                else:
+                    cur_row.append(pascal_tri[i - 1][j - 1] +
+                                   pascal_tri[i - 1][j])
 
-    for i in range(1, n):
-        prev_row = pascal_tri[-1]
-        cur_row = [1]
+            pascal_tri.append(cur_row)
 
-        for j in range(1, i):
-            cur_row.append(prev_row[j - 1] + prev_row[j])
-
-        cur_row.append(1)
-        pascal_tri.append(cur_row)
-
-    return pascal_tri
+    return (pascal_tri)
