047b5c19c0c010572d8c4ac3ca4d1f5a22134f0b
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-04-26 06:55:43 +0200

    Ditched Dynamic Programming approach

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-""" Minimum Operations: Using dynamic programming approach """
+"""
+Given a number n, write a method that calculates the fewest number
+of operations needed to result in exactly n H characters in the file.
+"""
+
 
 def minOperations(n: int) -> int:
     """
-    We initialize a DP table dp with size n + 1, where dp[i] represents the
-    minimum number of operations to achieve i characters.
+    Calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file.
+
+    Args:
+    - n (int): The target number of H characters.
+
+    Returns:
+    - int: The fewest number of operations.
     """
+    process = 2
+    op = 0
+    while n > 1:
+        while n % process == 0:
+            op += process
+            n /= process
+        process += 1
+    return op
 
-    if n == 1:
-        return 0  # It's already an 'H'
-
-    dp = [float('inf')] * (n + 1)  # Initialize DP array
-    dp[1] = 0  # Base case
-
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:  # Only consider factors
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n] if dp[n] != float('inf') else 0
