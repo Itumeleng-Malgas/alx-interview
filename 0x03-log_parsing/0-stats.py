1ecfc2d04aeb4290eec05c5755ee3f9233183c37
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-02 20:14:05 +0200

    Refractored

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """ Write a script that reads stdin line by line and computes metrics """
 
+from collections import defaultdict
+
+
+def report(fileSize, statusCodes):
+    """
+    Prints generated report to standard output
+    """
+    print("File size:", fileSize)
+    for key in sorted(statusCodes.keys(), key=int):
+        print("{}: {}".format(key, statusCodes[key]))
+
 
 def parseLogs():
     """
@@ -9,37 +20,29 @@ def parseLogs():
     stdin = __import__('sys').stdin
     lineNumber = 0
     fileSize = 0
-    statusCodes = {}
-    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    statusCodes = defaultdict(int)
+    codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
     try:
         for line in stdin:
             lineNumber += 1
-            line = line.split()
+            parts = line.split()
+            if len(parts) != 10:
+                continue  # Skip lines that do not match the expected format
             try:
-                fileSize += int(line[-1])
-                if line[-2] in codes:
-                    try:
-                        statusCodes[line[-2]] += 1
-                    except KeyError:
-                        statusCodes[line[-2]] = 1
-            except (IndexError, ValueError):
+                if parts[8] in codes:
+                    fileSize += int(parts[9])
+                    statusCodes[parts[8]] += 1
+            except ValueError:
                 pass
             if lineNumber == 10:
                 report(fileSize, statusCodes)
                 lineNumber = 0
+                fileSize = 0
+                statusCodes.clear()
         report(fileSize, statusCodes)
-    except KeyboardInterrupt as e:
+    except KeyboardInterrupt:
         report(fileSize, statusCodes)
         raise
-
-
-def report(fileSize, statusCodes):
-    """
-    Prints generated report to standard output
-    """
-    print("File size: {}".format(fileSize))
-    for key, value in sorted(statusCodes.items()):
-        print("{}: {}".format(key, value))
 
 
 if __name__ == '__main__':
