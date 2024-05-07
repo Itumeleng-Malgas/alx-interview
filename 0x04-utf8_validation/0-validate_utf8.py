16cda05800ff2621a6e6023ee7f2ae7249f4dc99
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-07 10:24:36 +0200

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,38 +6,34 @@ def validUTF8(data):
     """
     Method that determines if a given data set represents
     a valid UTF-8 encoding.
+
+    Shifting right say, by 5 positions effectively isolates the three most
+    significant bits (MSBs) of the byte.
+
+    Then checks if the three most significant bits of the byte match
+    the pattern 110, indicating that the byte is the start of a 2-byte
+    UTF-8 character
     """
 
-    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
-    # Mask to check if the most significant bit is set
-    mask1 = 1 << 7
-
-    # Mask to check if the second most significant bit is set
-    mask2 = 1 << 6
-
     for byte in data:
-        # If this is the start of a new character
         if num_bytes == 0:
-            # Count the number of bytes in this UTF-8 character
-            if byte & mask1 == 0:
+            if byte >> 5 == 0b110:
                 num_bytes = 1
-            elif byte & (mask1 >> 1) == 0b11000000:
+            elif byte >> 4 == 0b1110:
                 num_bytes = 2
-            elif byte & (mask1 >> 2) == 0b11100000:
+            elif byte >> 3 == 0b11110:
                 num_bytes = 3
-            elif byte & (mask1 >> 3) == 0b11110000:
-                num_bytes = 4
-            else:
+            elif byte >> 7 == 0b1:
+                # If the most significant bit (MSB) is set, indicating an
+                # invalid UTF-8 character, return False
                 return False
-            # Decrement num_bytes to handle the current byte
-            num_bytes -= 1
         else:
-            # Check if the byte is a continuation byte
-            if not (byte & mask1 and not (byte & mask2)):
+            # If num_bytes is not 0, check if the byte is a continuation byte
+            if byte >> 6 != 0b10:
                 return False
-            # Decrement num_bytes to indicate a continuation byte
+            # Decrement num_bytes to indicate processing of a continuation byte
             num_bytes -= 1
 
     return num_bytes == 0
