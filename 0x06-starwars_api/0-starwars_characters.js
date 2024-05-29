25c61328ae1242d45915b2bce48944846e772f5a
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-29 13:41:22 +0200

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
+// Import the 'request' module for making HTTP requests
 const request = require('request')
 
+// Get the movie ID from command line arguments
 const movieId = process.argv[2]
 
+// Define options for the HTTP request to the Star Wars API
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
   method: 'GET'
 }
 
-request(options, function(error, response, body){
+request(options, function(error, response, body) {
   if (!error) {
-    const character=JSON.parse(body).characters
+    const characters=JSON.parse(body).characters
     printCharacters(characters, 0)
   }
 })
 
 function printCharacters(characters, index) {
+  // Make a request to the character URL
   request(characters[index], function(error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name)
