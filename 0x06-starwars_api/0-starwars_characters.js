0111d4df90c4d7e7c3784f45439c82cfd1be9916
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-29 13:48:57 +0200

    0-starwars_characters.js removed comments

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-// Import the 'request' module for making HTTP requests
 const request = require('request')
 
-// Get the movie ID from command line arguments
 const movieId = process.argv[2]
 
-// Define options for the HTTP request to the Star Wars API
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
   method: 'GET'
@@ -19,12 +16,10 @@ request(options, function(error, response, body) {
 })
 
 function printCharacters(characters, index) {
-  // Make a request to the character URL
   request(characters[index], function(error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name)
       if (index + 1 < characters.length) {
-        // Recursive call to print the next character
         printCharacters(characters, index + 1)
       }
     }
